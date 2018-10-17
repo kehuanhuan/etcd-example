0584f985d13b8041f132e0de480baf6ad9c5d42a
kehuanhuan <huanhuan@yuanli-inc.com>
2018-10-17 09:57:13 +0800

    fix

--- etcd-service-discovery/example/main.go
+++ etcd-service-discovery/example/main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"github.com/kehuanhuan/etcd-service-discovery/discovery"
+    "flag"
+    "fmt"
+    "../discovery"
 )
 
 func main() {
-	var role 	   = flag.String("role", "", "master | worker")
-	var workerName = flag.String("workerName", "localhost", "this is worker name")
-	flag.Parse()
-	endpoints := []string{"http://172.16.238.101:2379"}
-	if *role == "master" {
-		master := discovery.NewMaster(endpoints)
-		master.WatchWorkers()
-	} else if *role == "worker" {
-		worker := discovery.NewWorker(*workerName, "127.0.0.1", endpoints)
-		worker.HeartBeat()
-	} else {
-		fmt.Println("example -h for usage")
-	}
+    var role       = flag.String("role", "", "master | worker")
+    var workerName = flag.String("workerName", "localhost", "this is worker name")
+    flag.Parse()
+    endpoints := []string{"http://172.16.238.101:2379"}
+    if *role == "master" {
+        master := discovery.NewMaster(endpoints)
+        master.WatchWorkers()
+    } else if *role == "worker" {
+        worker := discovery.NewWorker(*workerName, "127.0.0.1", endpoints)
+        worker.HeartBeat()
+    } else {
+        fmt.Println("example -h for usage")
+    }
 }
